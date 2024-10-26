df3d6b8ad597e5f591b8bc87e319b8aa733b0b4f
datots <dato_ts@hotmail.com>
2024-10-26 13:52:45 +0400

    NO TYPESCRIPT
    NO TYPESCRIPT

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,7 @@
 module.exports = {
-  extends: [
-    "plugin:react/recommended",
-    "plugin:@typescript-eslint/recommended",
-    "prettier",
-  ],
-  parser: "@typescript-eslint/parser",
-  plugins: ["react", "@typescript-eslint"],
+  extends: ["plugin:react/recommended", "prettier"],
+  parser: "babel-eslint", // Use babel-eslint to support JSX
+  plugins: ["react"],
   rules: {
     "react/react-in-jsx-scope": "off", // No need to import React when using JSX with React 17+
     "no-unused-vars": "warn",
